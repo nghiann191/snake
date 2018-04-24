f4baed6bf2f6c99f4e235f444cfbf0f648b0b8d8
zenon191 <37409773+zenon191@users.noreply.github.com>
2018-04-24 21:58:46 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,23 +7,25 @@
 using namespace std;
 #define board1 50
 #define board2 25
-enum TRANGTHAI{UP,DOWN,RIGHT,LEFT};
+enum TRANGTHAI{UP,DOWN,RIGHT,LEFT};			// liet ke cac trang thai cua ran
 
-struct toa_do{
+struct toa_do{								// toa do cua mot o trong man hinh console
 	int x,y;
+		
 };
 
 struct snake{
-	int n;
-	toa_do than[200];
+	int n;									// do dai than
+	toa_do than[200];						// so dot trong than ran
 	TRANGTHAI tt;
 };
 
-struct food{
-	toa_do food;
+struct food
+{
+	toa_do food;							// toa do cua thuc an
 };
 
-void begin(snake &snake,food &food)
+void begin(snake &snake,food &food)			// khoi tao toa do con ran ban dau voi 3 dot va toa do thuc an ban dau
 {
 	snake.n = 3;
 	snake.than[2].x=2;
@@ -38,28 +40,29 @@ void begin(snake &snake,food &food)
 	food.food.y=5;
 }
 
-void print_snake(snake snake,food food)
+void print_snake(snake snake,food food)		// hien thi con ran, thuc an, diem va khung tro choi
 {
-	for(int i=0 ; i<board2+1;i++)
+//	system("cls");
+	for(int i=0 ; i<board2+1;i++)			// duong ke doc cua khung
 	{
 		gotoXY(board1,i);
 		cout << char(179);
 	}
-	gotoXY(0,board2);
+	gotoXY(0,board2);						// duong ke ngang cua khung
 	for(int i=0;i<board1;i++)
 	{
 		cout << "_";
 	}
-	gotoXY(board1 + 1,0);
+	gotoXY(board1 + 1,0);								// diem tro choi
 	cout << "Scores: " << snake.n *10 - 30 ;
-	gotoXY(food.food.x,food.food.y);
+	gotoXY(food.food.x,food.food.y);					// hien thi thuc an
 	cout <<"$";
-	for(int i=0;i<snake.n;i++)
+	for(int i=0;i<snake.n;i++)							// hien thi con ran
 	{
 		gotoXY(snake.than[i].x,snake.than[i].y);
 		cout<< "o";
 	}
-	for(int i=snake.n ;i>0;i--)
+	for(int i=snake.n ;i>0;i--)							// xoa 1 dot duoi cuoi cung khi con ran di chuyen
 	{
 		gotoXY(snake.than[i].x , snake.than[i].y );
 		cout << " ";
@@ -67,83 +70,83 @@ void print_snake(snake snake,food food)
 	}
 }
 
-void control_moveSnake(snake &snake)
+void control_moveSnake(snake &snake)					// dieu khien su di chuyen cua con ran
 {
-	for(int i=snake.n;i>0;i--)
+	for(int i=snake.n;i>0;i--)							// ran di chuyen : dot truoc chuyen trang thai cho dot sau
 	{
 		snake.than[i].x=snake.than[i-1].x;
 		snake.than[i].y=snake.than[i-1].y;
 	}
 	
-	if(_kbhit())
+	if(_kbhit())										// dieu khien ran
 	{
-		if(checkKey(KEY_UP))
+		if(checkKey(KEY_UP))							// di len
 		{
 			if(snake.tt!=DOWN)
 				snake.tt = UP;
 		}
-		if(checkKey(KEY_DOWN))
+		if(checkKey(KEY_DOWN))							// di xuong
 		{
 			if(snake.tt!=UP)
 				snake.tt = DOWN;
 		}
-		if(checkKey(KEY_LEFT))
+		if(checkKey(KEY_LEFT))							// sang trai
 		{
 			if(snake.tt!=RIGHT)
 				snake.tt=LEFT;
 		}
-		if(checkKey(KEY_RIGHT))
+		if(checkKey(KEY_RIGHT))							// sang phai
 		{
 			if(snake.tt!=LEFT)
 				snake.tt=RIGHT;
 		}
 		
 	}
-	switch(snake.tt)
+	switch(snake.tt)									// tuy vao trang thai de di chuyen con ran
 	{
-		case UP: snake.than[0].y--;break;
-		case DOWN: snake.than[0].y++;break;
-		case RIGHT: snake.than[0].x++;break;
-		case LEFT: snake.than[0].x--;break;
+		case UP: snake.than[0].y--;break;				// khi ran di len thi toa do y giam
+		case DOWN: snake.than[0].y++;break;				// ran di xuong thi toa do y tang
+		case RIGHT: snake.than[0].x++;break;			// ran sang phai thi x tang
+		case LEFT: snake.than[0].x--;break;				// ran sang trai thi x giam
 	}		
 }
 
-void Processed_Main(snake &snake,food &food)
+void Processed_Main(snake &snake,food &food)			//xu ly chinh
 {
-	if(snake.than[0].x == food.food.x && snake.than[0].y == food.food.y)
+	if(snake.than[0].x == food.food.x && snake.than[0].y == food.food.y)		// ran an thuc an
 	{
-		snake.n++;
-		for(int i=snake.n;i>0;i--)
+		snake.n++;																// tang do dai
+		for(int i=snake.n;i>0;i--)												// dot moi la dau ran
 		{
 			snake.than[i] = snake.than[i-1];
 		}
-		food.food.x = 1+rand() % (board1-1);
+		food.food.x = 1+rand() % (board1-1);						// thuc an moi xuat hien ngau nhien
 		food.food.y = 1+rand() % (board2-1);
 	}
 }
 
-int GameOver(snake &snake)
+int GameOver(snake &snake)							// thua game, tra ve -1 neu thua game
 {
-	for(int i=4; i<snake.n  ;i++)
+	for(int i=4; i<snake.n  ;i++)					// ran tu can chinh minh
 	{
 		if(snake.than[0].x == snake.than[i].x && snake.than[0].y == snake.than[i].y )
 		{
 			return -1;
 		}
 	}
-	if(snake.than[0].x >= board1)
+	if(snake.than[0].x >= board1)					// ran dam tuong
 	{
 		return -1;
 	}
-	if(snake.than[0].x < 0)
+	if(snake.than[0].x < 0)							// ran dam tuong
 	{
 		return -1;
 	}
-	if(snake.than[0].y >= board2)
+	if(snake.than[0].y >= board2)					// ran dam tuong
 	{
 		return -1;
 	}
-	if(snake.than[0].y < 0)
+	if(snake.than[0].y < 0)							// ran dam tuong
 	{
 		return -1;
 	}
@@ -152,13 +155,13 @@ int GameOver(snake &snake)
 
 int main()
 {
-	srand(time(NULL));
+	srand(time(NULL));				// khoi tao bo sinh so ngau nhien
 	snake snake;
 	food food;
-	int num = 0;
+	int num = 0;					// num = 0 game chay bt, num = -1 game over
 	begin(snake,food);
 	
-	while(1)
+	while(1)						// vong lap chinh
 	{
 		print_snake(snake,food);
 		control_moveSnake(snake);
@@ -171,7 +174,7 @@ int main()
 			while(_getch() != 13);
 			break;
 		}
-		Sleep(100);
+		Sleep(100);					// thoi gian nghi 100ms
 	}
 	
 	return 0;
